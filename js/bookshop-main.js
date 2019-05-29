7f57e702649ad65e3c223b535d3f5f6d9828ae7e
galiliyo <yon.galili@gmail.com>
2019-05-29 22:27:38 +0300

    modal in progress

--- js/bookshop-main.js
+++ js/bookshop-main.js
@@ -1,122 +1,153 @@
 'use strict'
-// var gDeleteButtons
-// var gEditButtons
 
 function addEventListeners() {
-    $('.btn-delete').click(onDeletebook)
-    // gEditButtons = $('.btn-edit')
-    $('.btn-edit').click(onEditBook)
+  $('.btn-delete').click(onDeletebook)
 
-
+  $('.btn-edit').click(onEditBook)
 }
 
-// deleteButtons.forEach(btn => {
-//     btn.click(onDeletebook)
-// });
-
 function onInit() {
-
-    createBooks()
-    renderBooks()
-    addEventListeners()
+  createBooks()
+  renderBooks()
+  addEventListeners()
+  renderPagination()
 }
 
 function renderBooks() {
-    var books = getBooks()
-    var strHtmls = '<div class="row "> \n'
-    var mappedBooks = books.map(function (book) {
-        return `
-        <div class='col-md-4 col-lg-3'>
+  var books = getBooks()
+  var strHtmls = '<div class="row "> \n'
+  var mappedBooks = books.map(function(book) {
+    return `
+        <div class='col-sm-6 col-md-4 col-lg-3'>
             <div class="card" style="width: 14rem;">
-                <img class="card-img-top" src="${book.imgUrl}" alt="Card image cap">
+                <img class="card-img-top" src="${
+                  book.imgUrl
+                }" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title book-title"><a>${book.title}</a></h5>
-                    <h6 class="card-title author-title"><span>by </span>${book.author}</h6>
+                    <h6 class="card-title author-title"><span>by </span>${
+                      book.author
+                    }</h6>
                     <div class='card-controls'>
                         <div><h6>$ ${book.price}</h6></div>
                         <div>
-                            <a href="#" class="btn-edit btn-tertiary" data-toggle="modal" data-target="#editBookModal" data-id="${book.id}">
+                            <a href="#" class="btn-edit btn-tertiary" data-toggle="modal" data-target="#editBookModal" data-id="${
+                              book.id
+                            }">
                             <i class="far fa-edit"></i></a>
-                            <a href="#" class="btn-delete btn-tertiary" data-id="${book.id}"><i class="far fa-trash-alt"></i></a>
+                            <a href="#" class="btn-delete btn-tertiary" data-id="${
+                              book.id
+                            }"><i class="far fa-trash-alt"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>`
-    })
-    strHtmls += mappedBooks.join('') + '</div>'
+  })
+  strHtmls += mappedBooks.join('') + '</div>'
 
-    $('.cards').html(strHtmls)
+  $('.cards').html(strHtmls)
 }
 
 function onReadbook(bookId) {
-    readbook(bookId)
+  readbook(bookId)
 }
 
 function onCloseModal() {
-    $('.modal').hide()
+  $('.modal').hide()
 }
 
-function onSaveEditedBook(el){
-console.log(el)
+function onSaveEditedBook(el) {
+  console.log(el)
 }
 
 function onDeletebook(e) {
-    let bookId = $(this).data().id
-    deletebook(bookId)
-    renderBooks()
-    addEventListeners()
+  let bookId = $(this).data().id
+  deletebook(bookId)
+  renderBooks()
+  addEventListeners()
 }
 
 function onEditBook() {
-    let bookId = $(this).data().id
-    let currBook = getbookById(bookId)
-    console.log(currBook)
-    $('.modal #book-title').val(currBook.title)
-    $('.modal #book-author').val(currBook.author)
-    $('.modal #book-price').val(currBook.price)
-    $('.modal #book-overview').val(currBook.overview)
-
-
-
-
+  let bookId = $(this).data().id
+  let currBook = getbookById(bookId)
+  console.log(currBook)
+  $('.modal #book-title').val(currBook.title)
+  $('.modal #book-author').val(currBook.author)
+  $('.modal #book-price').val(currBook.price)
+  $('.modal #book-overview').val(currBook.overview)
 }
 
-
 function onAddBook() {
-    var title = prompt('Enter book Title')
-    var author = prompt('Enter book Author')
-    var price = prompt('Enter book Price')
-    var overview = prompt('Enter book Overview')
-    addbook(title, author, price, overview)
-    renderBooks()
-    addEventListeners()
+  var title = prompt('Enter book Title')
+  var author = prompt('Enter book Author')
+  var price = prompt('Enter book Price')
+  var overview = prompt('Enter book Overview')
+  addbook(title, author, price, overview)
+  renderBooks()
+  addEventListeners()
 }
 // Todo
 function onUpdatebook(bookId) {
-    var newSpeed = +prompt('Speed?');
-    updatebook(bookId, newSpeed);
-    renderBooks();
+  var newSpeed = +prompt('Speed?')
+  updatebook(bookId, newSpeed)
+  renderBooks()
 }
 
 function onNextPage() {
-    nextPage()
-    renderBooks()
+  nextPage()
+  renderBooks()
 }
 
 function readbook(bookId) {
-    var book = getbookById(bookId)
-    var $modal = $('.modal')
-    $modal.find('h5').text(book.vendor)
-    $modal.find('p').text(book.overview)
-    $modal.show()
-
+  var book = getbookById(bookId)
+  var $modal = $('.modal')
+  $modal.find('h5').text(book.vendor)
+  $modal.find('p').text(book.overview)
+  $modal.show()
 }
 
+function renderPagination() {
+  let currPageIdx = getCurrPageIdx()
+  let allBooks = getAllBooks()
+  let numberOfPages = Math.ceil(allBooks.length / PAGE_SIZE)
+  let strHtmls = `<li class="page-item">
+    <a class="page-link" data-page="prev" href="#" aria-label="Previous" onclick="onChangePage($(this).data('page'))">
+        <span aria-hidden="true">&laquo;</span>
+        <span class="sr-only">Previous</span>
+    </a>
+    </li> \n`
+  for (let i = 0; i < numberOfPages; i++) {
+      let strClass = (i===currPageIdx) ? 'active' : ''
+    strHtmls +=
+    `<li class="page-item ${strClass}">
+        <a class="page-link" data-page="${i}" onclick="onChangePage($(this).data('page'))">${i+1}</a>
+     </li>`
+  }
 
+  strHtmls += `<li class="page-item">
+  <a class="page-link" data-page="next" aria-label="Next" onclick="onChangePage($(this).data('page'))">
+    <span aria-hidden="true">&raquo;</span>
+    <span class="sr-only">Next</span>
+  </a>
+</li>`
 
+  $('ul.pagination').html(strHtmls)
+}
 
+function onChangePage(destinationPageIdx) {
+  let allBooks = getAllBooks()
+  let numberOfPages = Math.ceil(allBooks.length / PAGE_SIZE)
+  let curPageIdx = getCurrPageIdx()
+  if (destinationPageIdx === 'next') destinationPageIdx = curPageIdx + 1
+  else if (destinationPageIdx === 'prev') destinationPageIdx = curPageIdx - 1
 
-// {
-//     /* <p class="card-text">${book.overview}</p> */
-// }
+  if (destinationPageIdx > numberOfPages || destinationPageIdx < 0) {
+    destinationPageIdx = curPageIdx
+  }
+
+  setCurrPageIdx(destinationPageIdx)
+
+  renderBooks()
+  renderPagination()
+}
--- js/bookshop-service.js
+++ js/bookshop-service.js
@@ -1,68 +1,92 @@
 'use strict'
 
-var gBooks;
-const PAGE_SIZE = 10
+var gBooks
+const PAGE_SIZE = 6
 var currPageIdx = 0
 
-
 function getBooks() {
-    var fromIdx = currPageIdx * PAGE_SIZE
-    var books = gBooks.slice(fromIdx, fromIdx + PAGE_SIZE)
-    return books
+  var fromIdx = currPageIdx * PAGE_SIZE
+  var books = gBooks.slice(fromIdx, fromIdx + PAGE_SIZE)
+  return books
 }
 
+function getAllBooks(){
+    return gBooks
+}
 
 function createBooks() {
-    var books = [
-        createbook('Don Quixote', 'Miguel de Cervantes', '15.99'),
-        createbook('Moby Dick', 'Herman Melville', '15.99'),
-        createbook('The Great Gatsby', 'F. Scott Fitzgerald', '15.99'),
-        createbook('Hamlet', ' William Shakespeare', '15.99'),
-        createbook('War and Peace', 'Loe Tolstoy', '15.99')
-    ]
-    gBooks = books;
+  var books = [
+    createbook('Don Quixote', 'Miguel de Cervantes', '15.99', 4),
+    createbook('Moby Dick', 'Herman Melville', '15.99', 4),
+    createbook('The Great Gatsby', 'F. Scott Fitzgerald', '15.99', 4),
+    createbook('Hamlet', ' William Shakespeare', '15.99', 4),
+    createbook('War and Peace', 'Loe Tolstoy', '15.99', 4),
+    createbook('In Search of Lost Time', 'Marcel Proust', '15.99', 4),
+    createbook('Moby Dick', 'Herman Melville', '15.99', 4),
+    createbook('The Odyssey', 'Homer', '15.99', 4),
+    createbook('Hamlet', 'William Shakespeare', '15.99', 4),
+    createbook('One Hundred Years of Solitude', 'Gabriel Garcia Marquez', '15.99', 4),
+    createbook('The Divine Comedy', ' Dante Alighieri', '15.99', 4),
+    createbook('The Brothers Karamazov ', ' Fyodor Dostoyevsky', '15.99', 4),
+    createbook('Madame Bovary', 'Gustave Flaubert', '15.99', 4),
+    createbook('The Adventures of Huckleberry Finn', 'Mark Twain', '15.99', 4),
+    createbook('Lolita', 'Vladimir Nabokov', '15.99', 4)
+  ]
+  gBooks = books
 }
 
-function createbook(title, author, price, overview, imgUrl = './img/default-book.png') {
-    return {
-        id: makeId(),
-        title,
-        author,
-        price,
-        overview: 'The next I heard of Frank was that he was in Montana, and then he went prospecting in Arizona, and then I heard of him from New Mexico. After that came a long newspaper',
-        imgUrl,
-    }
+function createbook(
+  title,
+  author,
+  price,
+  overview,
+  imgUrl = './img/default-book.png'
+) {
+  return {
+    id: makeId(),
+    title,
+    author,
+    price,
+    overview:
+      'The next I heard of Frank was that he was in Montana, and then he went prospecting in Arizona, and then I heard of him from New Mexico. After that came a long newspaper',
+    imgUrl
+  }
 }
 
 function deletebook(bookId) {
-    var bookIdx = gBooks.findIndex(function (book) {
-        return bookId === book.id
-    })
-    gBooks.splice(bookIdx, 1)
+  var bookIdx = gBooks.findIndex(function(book) {
+    return bookId === book.id
+  })
+  gBooks.splice(bookIdx, 1)
 }
 
 function addbook(title, author, price, overview, imgUrl) {
-    var book = createbook(title, author, price, overview, imgUrl)
-    gBooks.push(book)
+  var book = createbook(title, author, price, overview, imgUrl)
+  gBooks.push(book)
 }
 
 function getbookById(bookId) {
-    var book = gBooks.find(function (book) {
-        return bookId === book.id
-    })
-    return book
+  var book = gBooks.find(function(book) {
+    return bookId === book.id
+  })
+  return book
 }
 
-
-
 function updatebook(bookId, title, author, imgUrl) {
-    var bookIdx = gBooks.findIndex(function (book) {
-        return book.id === bookId;
-        
-    })
-    // gBooks[bookIdx].  = newSpeed;
+  var bookIdx = gBooks.findIndex(function(book) {
+    return book.id === bookId
+  })
+  // gBooks[bookIdx].  = newSpeed;
 }
 
 function nextPage() {
-    currPageIdx++
+  currPageIdx++
+}
+
+function setCurrPageIdx(destinationPageIdx){
+    currPageIdx = destinationPageIdx
+}
+
+function getCurrPageIdx(){
+    return currPageIdx
 }
